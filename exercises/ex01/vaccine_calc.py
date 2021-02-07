a5bde311941bed47072be5f70183148b54e9da02
Ruperto Martinez <ponce11@email.unc.edu>
2021-02-06 20:23:04 -0500

    Completion of EX01

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -16,12 +16,29 @@ from datetime import datetime
 # Official Documentation: https://docs.python.org/3/library/datetime.html#datetime.timedelta
 from datetime import timedelta
 
-
 # Begin your solution here...
 
-population = input("Enter the total number of people under consideration.")
-doses_adminestered = input("Enter the total number of vaccine doses already given to the population.")
-doses_per_day = input("Enter the total number of vaccine doses given each calendar day.")
-percent_vaccinated = input("Enter what percent of the population you wish to know will be vaccinated.")
+population = int(input("Enter the total number of people under consideration:"))
+doses_adminestered = int(input("Enter the total number of vaccine doses already given to the population:"))
+doses_per_day = int(input("Enter the total number of vaccine doses given each calendar day:"))
+percent_vaccinated = int(input("Enter what percent of the population you wish to know will be vaccinated:"))
 
+amount_to_vaccinate = (percent_vaccinated)/100 * (population)
+#print(amount_to_vaccinate)
+people_vaccinated = (doses_adminestered)/2
+#print(people_vaccinated)
+people_vaccinated_per_day = (doses_per_day)/2
+#print(people_vaccinated_per_day)
+people_left = amount_to_vaccinate - (people_vaccinated)
+#print(people_left)
+days_left = round(people_left / people_vaccinated_per_day)
 
+today: datetime = datetime.today()
+fortnight: timedelta = timedelta(days_left)
+
+future: datetime = (today + fortnight)
+
+future_date = future.strftime("%B %d, %Y")
+
+print("We will reach " + str(percent_vaccinated) + "% vaccination in " + str(days_left) + " days, which falls on ", end="")
+print(future_date)
