c6a9ec4fa0331974191bb9a38ab9e2fa989baba2
Ruperto Martinez <ponce11@email.unc.edu>
2021-02-13 21:43:13 -0500

    Completion of EX02 Vaccine Calculator

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730408061"
 
 
 def main() -> None:
@@ -12,15 +12,38 @@ def main() -> None:
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
     # TODO 2: Call days_to_target and store result in a variable.
+    days = days_to_target(population, doses, doses_per_day, target)
     # TODO 4: Call future_date and store result in a variable.
+    future_string = future_date(days)
     # TODO 5: Print the expected output using the variables above.
+    print("We will reach " + str(target) + "% vaccination in " + str(days) + " days, which falls on " + future_string)
 
 
 # TODO 1: Define days_to_target function
+def days_to_target(population: int, doses: int, doses_per_day: int, target:int) -> int:
+    """Function to find number of days until target percent of population is vaccinated."""
+
+    amount_to_vaccinate = (target / 100) * (population)
+    people_vaccinated = (doses) / 2
+    people_vaccinated_per_day = (doses_per_day) / 2
+    people_left = amount_to_vaccinate - (people_vaccinated)
+    days_left = round(people_left / people_vaccinated_per_day)
+
+    return days_left
 
 
 # TODO 3: Define future_date function
+def future_date(days: int) -> str:
+    """Function to convert the number of days left into a written out date string."""
+    today: datetime = datetime.today()
 
+    fortnight: timedelta = timedelta(days)
+
+    future: datetime = (today + fortnight)
+
+    future_date = future.strftime("%B %d, %Y")
+
+    return future_date
 
 if __name__ == "__main__":
     main()
