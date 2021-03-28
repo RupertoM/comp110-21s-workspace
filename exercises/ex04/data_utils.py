4c21f3e207acfcc820c1512e350c3cb5d37f45ac
Ruperto Martinez <ponce11@email.unc.edu>
2021-03-28 18:54:43 -0400

    Progress on EX04

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -33,3 +33,48 @@ def column_values(rows_list, string):
         columns.append(row[string])
 
     return columns
+
+
+def columnar(list_of_dict):
+    """Function that transfroms a table of rows into a dict of columns."""
+    return_dict = {}
+    for row in list_of_dict:
+        for column in row:
+            return_dict[column] = column_values(list_of_dict, column)
+        
+    return return_dict
+
+
+def head(dictionary, number):
+    """Prodcues new column-based table with only first N rows of data."""
+    return_dictionary = {}
+    for column in dictionary:
+        empty_list = []
+        for i in range(number):
+            empty_list.append(dictionary[column][i])
+            return_dictionary[column] = empty_list
+
+    return return_dictionary
+            
+
+def select(dictionary, list_columns):
+    """A function that produces a new column based table with specific subsets."""
+    return_dictionary = {}
+    for column in list_columns:
+        return_dictionary[column] = dictionary[column]
+
+    return return_dictionary
+
+
+def count(list_of_strings):
+    """Given a list function produces a dictionary and counts amount of times unique values appear."""
+    return_dictionary = {}
+    for item in list_of_strings:
+        if item in return_dictionary:
+            item_count += 1
+        else:
+            item_count = 1
+        return_dictionary[item] = item_count
+    
+    return return_dictionary
+
