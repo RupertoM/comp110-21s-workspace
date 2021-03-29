a21a743bfcadbb292a79c5cb0c7751328be269aa
Ruperto Martinez <ponce11@email.unc.edu>
2021-03-28 22:56:29 -0400

    Completion of EX04 (no corrections)

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -6,7 +6,7 @@ __author__ = "730408061"
 from csv import DictReader
 
 
-def read_csv_rows(csv_file: str):
+def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
     rows: list[dict[str, str]] = []
 
@@ -25,8 +25,8 @@ def read_csv_rows(csv_file: str):
 
 # TODO: Define the other functions here.
 
-def column_values(rows_list, string):
-    """A function that produces all values in a list into a single column"""
+def column_values(rows_list: list[dict[str, str]], string: str) -> list[str]:
+    """A function that produces all values in a list into a single column."""
     columns = []
 
     for row in rows_list:
@@ -35,7 +35,7 @@ def column_values(rows_list, string):
     return columns
 
 
-def columnar(list_of_dict):
+def columnar(list_of_dict: list[dict[str, str]]) -> dict[str, list[str]]:
     """Function that transfroms a table of rows into a dict of columns."""
     return_dict = {}
     for row in list_of_dict:
@@ -45,19 +45,24 @@ def columnar(list_of_dict):
     return return_dict
 
 
-def head(dictionary, number):
+def head(dictionary: dict[str, list[str]], number: int) -> dict[str, list[str]]:
     """Prodcues new column-based table with only first N rows of data."""
     return_dictionary = {}
     for column in dictionary:
         empty_list = []
-        for i in range(number):
-            empty_list.append(dictionary[column][i])
+        if number == 0:
             return_dictionary[column] = empty_list
+        if number > len(dictionary[column]):
+            return_dictionary[column] = dictionary[column]
+        else:
+            for i in range(number):
+                empty_list.append(dictionary[column][i])
+                return_dictionary[column] = empty_list
 
     return return_dictionary
             
 
-def select(dictionary, list_columns):
+def select(dictionary: dict[str, list[str]], list_columns: list[str]) -> dict[str, list[str]]:
     """A function that produces a new column based table with specific subsets."""
     return_dictionary = {}
     for column in list_columns:
@@ -66,15 +71,15 @@ def select(dictionary, list_columns):
     return return_dictionary
 
 
-def count(list_of_strings):
+def count(list_of_strings: list[str]) -> dict[str, int]:
     """Given a list function produces a dictionary and counts amount of times unique values appear."""
     return_dictionary = {}
+    count = 0
     for item in list_of_strings:
         if item in return_dictionary:
-            item_count += 1
+            count += 1
         else:
-            item_count = 1
-        return_dictionary[item] = item_count
+            count = 1
+        return_dictionary[item] = count
     
     return return_dictionary
-
