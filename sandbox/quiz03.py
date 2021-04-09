822c83474986771f29cfb41c41362d04a2bccf6c
Ruperto Martinez <ponce11@email.unc.edu>
2021-04-09 18:13:24 -0400

    Standard Upload for sandbox work

---
+++ sandbox/quiz03.py
@@ -0,0 +1,15 @@
+
+
+
+list1 = [2]
+list2 = list1
+list2[0] = 3
+
+print(list1)
+
+example_dict = {"response": [1.0, 2.0]}
+for i in example_dict:
+    print(i)
+a = [1,5,3,2]
+for i in range(len(a)):
+    print(i)
